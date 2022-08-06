109a34823ea240cf72d52410f9fb8a0b711cf7a8
Soham Gangopadhyay <sohamgangopadhyay@Sohams-MacBook-Pro.local>
2022-08-06 16:01:07 -0400

    New App chnages

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
-def main( ):
-    print('Hello Wold')
+import pandas as pd
+
+
+def main():
+    # The file name is hardcoded and assigned to fp.
+    fp = '/Users/sohamgangopadhyay/Projects/Internal/Bootcamp/Data-Copier/Data/retail_db_json/order_items/part-r-00000-6b83977e-3f20-404b-9b5f-29376ab1419e'
+
+    df = pd.read_json(fp, lines=True)
+
+    df.count()
+    # df.describe()
+    #
+    # df.columns
+    # df.dtypes
+    #
+    # df[['order_item_order_id', 'order_item_subtotal']]
+    #
+    print(df[df['order_item_order_id'] == 2])
+
 
 if __name__ == "__main__":
     main()
