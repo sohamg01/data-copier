bc74a71cb8f23f1f10b1a7337bc4511bce043232
sohamg01 <sohamgangopadhyay@yahoo.com>
2022-08-06 22:27:15 -0400

    creating base directory and adding to path

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import pandas as pd
-
+import os
 
 def main():
     # The file name is hardcoded and assigned to fp.
-    fp = '/Users/sohamgangopadhyay/Projects/Internal/Bootcamp/Data-Copier/Data/retail_db_json/order_items/part-r-00000-6b83977e-3f20-404b-9b5f-29376ab1419e'
+    #fp = '/Users/sohamgangopadhyay/Projects/Internal/Bootcamp/Data-Copier/Data/retail_db_json/order_items/part-r-00000-6b83977e-3f20-404b-9b5f-29376ab1419e'
+
+    BASE_DIR = '/Users/sohamgangopadhyay/Projects/Internal/Bootcamp/Data-Copier/Data/retail_db_json'
+    table_name = 'order_items'
+
+    # Lists all the files in the folder.
+    # All our folders contain only one file.
+    # Hence, we can access it by reading first element in the list as below.
+
+    file_name = os.listdir(f'{BASE_DIR}/{table_name}')[0]
+    fp = f'{BASE_DIR}/{table_name}/{file_name}'
 
     df = pd.read_json(fp, lines=True)
     # Here is the piece of code to read the content of the file as reader.
