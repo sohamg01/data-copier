ffaf621a04a296137ce2b6f2155ce02e33a07c85
sohamg01 <sohamgangopadhyay@yahoo.com>
2022-08-06 22:14:57 -0400

    reading in chunks

--- app.py
+++ app.py
@@ -6,16 +6,12 @@ def main():
     fp = '/Users/sohamgangopadhyay/Projects/Internal/Bootcamp/Data-Copier/Data/retail_db_json/order_items/part-r-00000-6b83977e-3f20-404b-9b5f-29376ab1419e'
 
     df = pd.read_json(fp, lines=True)
+    # Here is the piece of code to read the content of the file as reader.
+    json_reader = pd.read_json(fp, lines=True, chunksize=1000)
 
-    df.count()
-    # df.describe()
-    #
-    # df.columns
-    # df.dtypes
-    #
-    # df[['order_item_order_id', 'order_item_subtotal']]
-    #
-    print(df[df['order_item_order_id'] == 2])
+    # Here is the piece of code to read each chunk as Dataframe.
+    for idx, df in enumerate(json_reader):
+        print(f'Number of records in chunk with index {idx} is {df.shape[0]}')
 
 
 if __name__ == "__main__":
